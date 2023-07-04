ad7896ac489a4b2ade804ad3c54a84b67a552798
Yixin <yixin88@hotmail.co.uk>
2023-07-04 17:38:07 +0100

    hooked up select and search input to generate dynamic url

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,8 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/solid"
 import SearchButton from "./SearchButton"
 import { SearchSelect, SearchSelectItem, Select, SelectItem } from "@tremor/react"
 import Avatar from "react-avatar"
+import { useState } from "react"
+import { useRouter } from "next/navigation"
 
 const SORT_BY_MAP = {
   r: "Default",
@@ -15,6 +17,14 @@ const SORT_BY_MAP = {
 }
 
 function Header() {
+
+  const [pages, setPages] = useState("");
+  const [sortBy, setSortBy] = useState("");
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
+
+  const router = useRouter();
+
   return (
     <header className="flex flex-col items-center md:flex-row md:items-start md:space-x-6 px-2 pt-10 pb-5 md:p-10 md:pb-5">
       <Link href={"/"}>
@@ -22,7 +32,20 @@ function Header() {
       </Link>
 
       <div className="w-full md:max-w-2xl">
-        <form action="">
+        <form action={formData => {
+          const searchTerm = formData.get("searchTerm");
+
+          if (!searchTerm) return;
+
+          const params = new URLSearchParams();
+
+          if (pages) params.set("pages", pages.toString());
+          if (sortBy) params.set("sort_by", sortBy.toString());
+          if (minPrice) params.set("min_price", minPrice.toString());
+          if (maxPrice) params.set("max_price", maxPrice.toString());
+
+          router.push(`/search/${searchTerm}?${params.toString()}`);
+        }}>
           <div className="flex items-center gap-2 w-full px-4">
             <div className="flex items-center space-x-2 bg-white shadow-xl rounded-full border-0 px-6 py-4 flex-1">
               <MagnifyingGlassIcon className="h-5 w-5 text-gray-400"/>
@@ -33,7 +56,7 @@ function Header() {
           </div>
 
           <div className="grid grid-cols-2 gap-2 p-4 md:grid-cols-4 max-w-lg md:max-w-none mx-auto item-center">
-            <SearchSelect className="min-w-4" placeholder="# of pages">
+            <SearchSelect onValueChange={value => setPages(value)} className="min-w-4" placeholder="# of pages">
               {[...Array(100)].map((_, i) => (
                 <SearchSelectItem key={i} value={(i+1).toString()}>
                   {(i+1).toString()} pages
@@ -41,13 +64,13 @@ function Header() {
               ))}
             </SearchSelect>
 
-            <Select className="min-w-4" placeholder="Sort">
+            <Select onValueChange={value => setSortBy(value)} className="min-w-4" placeholder="Sort">
               {Object.entries(SORT_BY_MAP).map(([key, value]) => (
                 <SelectItem key={key} value={key}>{value}</SelectItem>
               ))}
             </Select>
 
-            <SearchSelect className="min-w-4" placeholder="Min Price...">
+            <SearchSelect onValueChange={value => setMinPrice(value)} className="min-w-4" placeholder="Min Price...">
               {["", "100", "250", "500", "750", "900", "1000+"].map((val, i) => (
                 <SearchSelectItem key={i} value={val.toString()}>
                   {i === 0 ? "No Minimum" : `$${val.toString()}`}
@@ -55,7 +78,7 @@ function Header() {
               ))}
             </SearchSelect>
 
-            <SearchSelect className="min-w-4" placeholder="Max Price...">
+            <SearchSelect onValueChange={value => setMaxPrice(value)} className="min-w-4" placeholder="Max Price...">
               {["", "100", "250", "500", "750", "900", "1000+"].map((val, i) => (
                 <SearchSelectItem key={i} value={val.toString()}>
                   {i === 0 ? "No Maximum" : `$${val.toString()}`}

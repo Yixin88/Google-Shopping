563cdf835d62dfb419049b10ba2979718b02a837
Yixin <yixin88@hotmail.co.uk>
2023-07-04 17:13:39 +0100

    header finished with styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,19 @@ import Image from "next/image"
 import Link from "next/link"
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid"
 import SearchButton from "./SearchButton"
+import { SearchSelect, SearchSelectItem, Select, SelectItem } from "@tremor/react"
+import Avatar from "react-avatar"
+
+const SORT_BY_MAP = {
+  r: "Default",
+  rv: "By Review",
+  p: "By Price (low to high)",
+  pd: "By Price (high to low)"
+}
 
 function Header() {
   return (
-    <header>
+    <header className="flex flex-col items-center md:flex-row md:items-start md:space-x-6 px-2 pt-10 pb-5 md:p-10 md:pb-5">
       <Link href={"/"}>
         <Image src="https://links.papareact.com/208" alt="Google Logo" width={150} height={150} className="object-contain mr-10"/>
       </Link>
@@ -21,9 +30,45 @@ function Header() {
             </div>
 
             <SearchButton />
+          </div>
+
+          <div className="grid grid-cols-2 gap-2 p-4 md:grid-cols-4 max-w-lg md:max-w-none mx-auto item-center">
+            <SearchSelect className="min-w-4" placeholder="# of pages">
+              {[...Array(100)].map((_, i) => (
+                <SearchSelectItem key={i} value={(i+1).toString()}>
+                  {(i+1).toString()} pages
+                </SearchSelectItem>
+              ))}
+            </SearchSelect>
+
+            <Select className="min-w-4" placeholder="Sort">
+              {Object.entries(SORT_BY_MAP).map(([key, value]) => (
+                <SelectItem key={key} value={key}>{value}</SelectItem>
+              ))}
+            </Select>
+
+            <SearchSelect className="min-w-4" placeholder="Min Price...">
+              {["", "100", "250", "500", "750", "900", "1000+"].map((val, i) => (
+                <SearchSelectItem key={i} value={val.toString()}>
+                  {i === 0 ? "No Minimum" : `$${val.toString()}`}
+                </SearchSelectItem>
+              ))}
+            </SearchSelect>
+
+            <SearchSelect className="min-w-4" placeholder="Max Price...">
+              {["", "100", "250", "500", "750", "900", "1000+"].map((val, i) => (
+                <SearchSelectItem key={i} value={val.toString()}>
+                  {i === 0 ? "No Maximum" : `$${val.toString()}`}
+                </SearchSelectItem>
+              ))}
+            </SearchSelect>
 
           </div>
         </form>
+      </div>
+
+      <div className="hidden lg:flex flex-1 justify-end">
+        <Avatar name="Yixin Zhang" round size="50"/>
       </div>
     </header>
   )

a0a90264e89ee0e3060ca858e8457679252b084c
Yixin <yixin88@hotmail.co.uk>
2023-07-05 16:00:46 +0100

    fixed function

--- lib/getFetchUrl.ts
+++ lib/getFetchUrl.ts
@@ -1,7 +1,7 @@
-export const getFetchUrl = (route: string) => {
+export const getFetchUrl = (route: string) => (
   `${
     process.env.NODE_ENV === 'production'
     ? process.env.VERCEL_URL!
     : "http://localhost:3000"
   }/${route}`
-}
+)

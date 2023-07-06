d191771949247117f0cc18b5625340746c63895f
Yixin <yixin88@hotmail.co.uk>
2023-07-06 18:34:11 +0100

    moved shopping route to app

---
+++ app/shopping/product/[id]/page.tsx
@@ -0,0 +1,32 @@
+import { getFetchUrl } from "@/lib/getFetchUrl";
+import { ProductData } from "@/typings";
+import { StarIcon } from "@heroicons/react/24/solid";
+import { notFound } from "next/navigation";
+
+export const revalidate = 300;
+
+type ProductPageProps = {
+  params: {
+    id: string
+  }
+}
+
+async function ProductPage({params: { id }}: ProductPageProps) {
+
+  const response = await fetch(getFetchUrl(`api/shopping/product/${id}`));
+
+  const productData = await response.json() as ProductData;
+
+  if (!productData.content.pricing) {
+    notFound();
+  }
+
+
+  return (
+    <div className="p-12 pt-0">
+      <h1 className="text-2xl">{productData.content.title}</h1>
+    </div>
+  )
+}
+
+export default ProductPage
